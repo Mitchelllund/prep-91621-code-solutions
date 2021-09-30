c5b90e05807568046841bc0534ad74c25a20f2b5
Mitchell Lund <mitchlund922@gmail.com>
2021-09-29 22:18:37 -0700

    Completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+var firstelement = languages[0];
+console.log('the first element of the array is', firstelement);
+
+var thirdelement = languages[3];
+console.log('the 3rd element of the array is', thirdelement);
+
+var length = languages.length;
+console.log('the length of the array', length);
+
+var lastelement = languages[languages.length - 1];
+console.log('the last element of the array is', lastelement);
